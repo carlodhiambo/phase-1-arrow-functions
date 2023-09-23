cfce66573581d1a4801ea1b891944cc31c8e7743
Carlwilliam <jdcarlwilliam@gmail.com>
2023-09-23 21:39:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const Divide = (num1, num2)=>num1/num2;
-Divide(2000/100);
-console.log(result);
+
+function divide() {
+    return 2000 / 100;
+  };
+  
+  const square = (number) => {
+    return number * number;
+  };
+  
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+ 
+  
